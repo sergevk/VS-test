2574597e5ea6dfce9b1781d1d4ad407c7e1c6afa
Sergey Krandin <sergevk@hotmail.com>
2016-05-02 04:00:16 +0300

    initial commit

---
+++ PowerShellProject1/vs-test.ps1
@@ -0,0 +1,18 @@
+﻿#
+# Script.ps1
+#
+
+$some_things = "first", "second", "third"
+
+[string[]] $other_things = "first", "seconds", "third"
+
+[int[]] $more_things = 1,2,3,4
+
+$c = Get-ChildItem 'c:\' | where {$_.name -eq 'amd'}
+$d = Get-Service | where {$_.Name -eq 'wuauserv'}
+
+#$d
+
+Write-Output $d
+
+Write-Output "Break here"  ## Breakpoint here
